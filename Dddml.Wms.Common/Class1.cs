8875fc22f91c83760fd336b7ef5c345dc9bd8181
wubuku <yangjiefeng@gmail.com>
2016-05-13 21:14:48 +0800

    提交 WMS 的初始项目文件。

---
+++ Dddml.Wms.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dddml.Wms.Common
+{
+    public class Class1
+    {
+    }
+}

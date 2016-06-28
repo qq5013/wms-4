f909e033a8fc2c036fd301d4e7e30d890c1ede1d
wubuku <yangjiefeng@gmail.com>
2016-06-28 18:57:46 +0800

    改进了项目相关的 TypeConverter 的实现。减少重复代码。

--- Dddml.Wms.Common/Specialization/DefaultTypeConverter.cs
+++ Dddml.Wms.Common/Specialization/DefaultTypeConverter.cs
@@ -5,16 +5,16 @@ namespace Dddml.Wms.Specialization
 {
     public class DefaultTypeConverter : ITypeConverter
     {
+        global::Dddml.Support.Criterion.DefaultTypeConverter _innerTypeConverter = new global::Dddml.Support.Criterion.DefaultTypeConverter();
+
         public virtual object ConvertFromString(Type type, string text)
         {
-            var converter = TypeDescriptor.GetConverter(type);
-            return converter.ConvertFromString(text);
+            return _innerTypeConverter.ConvertFromString(type, text);
         }
 
         public virtual string ConvertToString(Type type, object value)
         {
-            var converter = TypeDescriptor.GetConverter(type);
-            return converter.ConvertToString(value);
+            return _innerTypeConverter.ConvertToString(type, value);
         }
     }
 }

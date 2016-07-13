3f6ef4bb1ec311c40854016e51157e73e4bfc924
wubuku <yangjiefeng@gmail.com>
2016-07-13 20:49:28 +0800

    将 PHP 的元数据放到单独的命名空间去。

---
+++ Dddml.Wms.HttpServices.PhpClient/Wms/Domain/Metadata/AttributeMetadata.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+namespace Wms\Domain\Metadata;
+
+
+class AttributeMetadata
+{
+    const VERSION      = 'version';
+    const ACTIVE       = 'active';
+    const DELETED      = 'deleted';
+    const CREATED_BY   = 'createdBy';
+    const CREATED_AT   = 'createdAt';
+    const UPDATED_BY   = 'updatedBy';
+    const UPDATED_AT   = 'updatedAt';
+}
+

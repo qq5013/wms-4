10885ebd948ce6914d291bb26ef74b8adc7c868d
wubuku <yangjiefeng@gmail.com>
2016-07-16 16:36:42 +0800

    增加 Java Client

---
+++ Dddml.Wms.HttpServices.JavaClient/src/org/dddml/wms/TestMain.java
@@ -0,0 +1,13 @@
+package org.dddml.wms;
+
+import org.dddml.wms.domain.meta.AttributeMetadata;
+
+/**
+ * Created by Yang on 2016/7/16.
+ */
+public class TestMain {
+    public static void main(String[] args)
+    {
+        System.out.println(AttributeMetadata.ACTIVE);
+    }
+}
---
+++ Dddml.Wms.HttpServices.JavaClient/src/org/dddml/wms/domain/meta/UserRoleMvoMetadata.java
@@ -0,0 +1,14 @@
+package org.dddml.wms.domain.meta;
+
+
+public interface UserRoleMvoMetadata
+{
+    String VERSION      = "userVersion";
+    String ACTIVE       = "active";
+    String DELETED      = "deleted";
+    String CREATED_BY   = "createdBy";
+    String CREATED_AT   = "createdAt";
+    String UPDATED_BY   = "updatedBy";
+    String UPDATED_AT   = "updatedAt";
+}
+

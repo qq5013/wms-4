6b290b32af8f182f0a8dbee15b924e641c53e0c3
wubuku <yangjiefeng@gmail.com>
2016-07-18 18:14:44 +0800

    提交了 Java Money Dto 类的代码。

--- Dddml.Wms.HttpServices.JavaClient/src/nodamoney/Money.java
+++ Dddml.Wms.HttpServices.JavaClient/src/nodamoney/Money.java
@@ -1,7 +1,30 @@
 package nodamoney;
 
+import java.math.BigDecimal;
+
 /**
  * Created by Yang on 2016/7/16.
  */
 public class Money {
+
+    private BigDecimal amount;
+
+    private String currency;
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
 }

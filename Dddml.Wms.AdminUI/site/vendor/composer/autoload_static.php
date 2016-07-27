905e0471d3d188c8130744805307440b66e030e1
dongww <dongww@gmail.com>
2016-07-27 15:30:11 +0800

    更新第三方类库目录结构，调整框架目录结构

--- Dddml.Wms.AdminUI/site/vendor/composer/autoload_static.php
+++ Dddml.Wms.AdminUI/site/vendor/composer/autoload_static.php
@@ -12,9 +12,16 @@ class ComposerStaticInit9001c896d4b5cae3c3a3862f84451521
         '023d27dca8066ef29e6739335ea73bad' => __DIR__ . '/..' . '/symfony/polyfill-php70/bootstrap.php',
         '6a47392539ca2329373e0d33e1dba053' => __DIR__ . '/..' . '/symfony/polyfill-intl-icu/bootstrap.php',
         'bd9634f2d41831496de0d3dfe4c94881' => __DIR__ . '/..' . '/symfony/polyfill-php56/bootstrap.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
+        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
     public static $prefixLengthsPsr4 = array (
+        'W' => 
+        array (
+            'Wms\\' => 4,
+        ),
         'S' => 
         array (
             'Symfony\\Polyfill\\Util\\' => 22,
@@ -50,13 +57,32 @@ class ComposerStaticInit9001c896d4b5cae3c3a3862f84451521
             'Symfony\\Bridge\\Monolog\\' => 23,
             'Silex\\' => 6,
         ),
+        'P' => 
+        array (
+            'Psr\\Http\\Message\\' => 17,
+        ),
         'M' => 
         array (
             'Monolog\\' => 8,
         ),
+        'G' => 
+        array (
+            'GuzzleHttp\\Psr7\\' => 16,
+            'GuzzleHttp\\Promise\\' => 19,
+            'GuzzleHttp\\' => 11,
+        ),
+        'D' => 
+        array (
+            'Doctrine\\Instantiator\\' => 22,
+            'Dddml\\' => 6,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'Wms\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/../../Dddml.Wms.HttpServices.PhpClient/Wms',
+        ),
         'Symfony\\Polyfill\\Util\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-util',
@@ -185,10 +211,38 @@ class ComposerStaticInit9001c896d4b5cae3c3a3862f84451521
         array (
             0 => __DIR__ . '/..' . '/silex/silex/src/Silex',
         ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
         'Monolog\\' => 
         array (
             0 => __DIR__ . '/..' . '/monolog/monolog/src/Monolog',
         ),
+        'GuzzleHttp\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+        ),
+        'GuzzleHttp\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
+        ),
+        'GuzzleHttp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
+        ),
+        'Doctrine\\Instantiator\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/instantiator/src/Doctrine/Instantiator',
+        ),
+        'Dddml\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/../../Dddml.Wms.HttpServices.PhpClient/src/Dddml',
+        ),
+    );
+
+    public static $fallbackDirsPsr4 = array (
+        0 => __DIR__ . '/../..' . '/src',
     );
 
     public static $prefixesPsr0 = array (
@@ -216,11 +270,48 @@ class ComposerStaticInit9001c896d4b5cae3c3a3862f84451521
             array (
                 0 => __DIR__ . '/..' . '/pimple/pimple/src',
             ),
+            'PhpOption\\' => 
+            array (
+                0 => __DIR__ . '/..' . '/phpoption/phpoption/src',
+            ),
+            'PhpCollection' => 
+            array (
+                0 => __DIR__ . '/..' . '/phpcollection/phpcollection/src',
+            ),
+        ),
+        'M' => 
+        array (
+            'Metadata\\' => 
+            array (
+                0 => __DIR__ . '/..' . '/jms/metadata/src',
+            ),
+        ),
+        'J' => 
+        array (
+            'JMS\\Serializer' => 
+            array (
+                0 => __DIR__ . '/..' . '/jms/serializer/src',
+            ),
+            'JMS\\' => 
+            array (
+                0 => __DIR__ . '/..' . '/jms/parser-lib/src',
+            ),
+        ),
+        'D' => 
+        array (
+            'Doctrine\\Common\\Lexer\\' => 
+            array (
+                0 => __DIR__ . '/..' . '/doctrine/lexer/lib',
+            ),
+            'Doctrine\\Common\\Annotations\\' => 
+            array (
+                0 => __DIR__ . '/..' . '/doctrine/annotations/lib',
+            ),
         ),
     );
 
     public static $fallbackDirsPsr0 = array (
-        0 => __DIR__ . '/../..' . '/src',
+        0 => __DIR__ . '/../..' . '/include',
     );
 
     public static $classMap = array (
@@ -241,6 +332,7 @@ class ComposerStaticInit9001c896d4b5cae3c3a3862f84451521
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit9001c896d4b5cae3c3a3862f84451521::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit9001c896d4b5cae3c3a3862f84451521::$prefixDirsPsr4;
+            $loader->fallbackDirsPsr4 = ComposerStaticInit9001c896d4b5cae3c3a3862f84451521::$fallbackDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit9001c896d4b5cae3c3a3862f84451521::$prefixesPsr0;
             $loader->fallbackDirsPsr0 = ComposerStaticInit9001c896d4b5cae3c3a3862f84451521::$fallbackDirsPsr0;
             $loader->classMap = ComposerStaticInit9001c896d4b5cae3c3a3862f84451521::$classMap;
--- Dddml.Wms.HttpServices.PhpClient/example/wms_command_bootstrap.php
+++ Dddml.Wms.HttpServices.PhpClient/example/wms_command_bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../Dddml.Wms.AdminUI/site/vendor/autoload.php';
 
 use Dddml\Executor\Http\CommandExecutor;
 use Doctrine\Common\Annotations\AnnotationRegistry;

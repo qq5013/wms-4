7809e8213573da7d537d2f68f893aefda9cded3d
dongww <dongww@gmail.com>
2016-08-05 13:52:51 +0800

    新增 vue 下拉按钮组件和 webpack 入口文件

---
+++ Dddml.Wms.AdminUI/webpack/components/bootstrap/buttonDropdowns.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="btn-group">
+        <button type="button"
+                class="btn {{btnType}} dropdown-toggle"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false">
+            {{text}}<span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu">
+            <li v-for="link in links">
+                <a href="{{link.href}}" target="{{link.target ? link.target : '_self'}}">{{link.text}}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+<style>
+</style>
+<script>
+    export default{
+        data(){
+            return {}
+        },
+        props: {
+            text: String,
+            btnType: String,
+            links: Array
+        }
+    }
+</script>
---
+++ Dddml.Wms.AdminUI/webpack/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import buttonDropdowns from './components/bootstrap/buttonDropdowns.vue'
+
+new Vue({
+    el: 'body',
+    components: {
+        buttonDropdowns
+    }
+});

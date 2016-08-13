b701a5ededf13be2429b4085f756abef62d922cd
dongww <dongww@gmail.com>
2016-08-13 15:23:23 +0800

    更改详情页标题

--- Dddml.Wms.AdminUI/webpack/components/Entity.vue
+++ Dddml.Wms.AdminUI/webpack/components/Entity.vue
@@ -3,7 +3,7 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <h1>
-                {{$route.params.name}}:{{$route.params.id}}
+                {{metadata.label}}
             </h1>
             <ol class="breadcrumb">
                 <li><a href="#"><i class="fa fa-dashboard"></i> 首页</a></li>
@@ -20,7 +20,7 @@
             <!-- Default box -->
             <div class="box">
                 <div class="box-header with-border">
-                    <h3 class="box-title">标题</h3>
+                    <h3 class="box-title">{{metadata.label}}</h3>
                 </div>
                 <div class="box-body form-horizontal">
                     <div class="form-group">
